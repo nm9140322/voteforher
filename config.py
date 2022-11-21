eee65dc0e516d923841471a60f4e0b86dd62b27c
nm9140322 <nm9140322@gmail.com>
2022-11-21 21:35:50 +0800

    voteforher

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 pjdir = os.path.abspath(os.path.dirname(__file__))
 def create_sqlite_uri(db_name):
     return "sqlite:///" + os.path.join(pjdir, db_name)
-DATABASE_URI = "postgres://evqspvzoibbyph:0ea42790b514f2dee5d1414ed82ef6551fa5057f50002f04bc764ce15cd7a423@ec2-54-86-214-124.compute-1.amazonaws.com:5432/dd7qq3kai8fuk7"
+DATABASE_URI = "postgresql://evqspvzoibbyph:0ea42790b514f2dee5d1414ed82ef6551fa5057f50002f04bc764ce15cd7a423@ec2-54-86-214-124.compute-1.amazonaws.com:5432/dd7qq3kai8fuk7"
 
 class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False # True會追蹤各種改變的信號而消耗額外的記憶體
@@ -18,18 +18,18 @@ class BaseConfig:
 class DevelopmentConfig(BaseConfig): # 開發環境
     DEBUG = True
     # SQLALCHEMY_DATABASE_URI = create_sqlite_uri('app\\static\\database\\voteforher_login.sqlite')
-    SQLALCHEMY_DATABASE_URI = "postgres://evqspvzoibbyph:0ea42790b514f2dee5d1414ed82ef6551fa5057f50002f04bc764ce15cd7a423@ec2-54-86-214-124.compute-1.amazonaws.com:5432/dd7qq3kai8fuk7"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
     # 終端機設置 $env:DATABASE_URI = "postgres://HEROKU的API" # HEROKU部署-pg
 
 class TestingConfig(BaseConfig): # 測試環境
     TESTING = True
     WTF_CSRF_ENABLED = False # flask-wtf 用 csrf_token處理 CSRF 的攻擊，測試時不會像正常使用一樣實際按下送出，而是直接傳資料到後端會被 csrf_token 擋下來，所以要關掉。
     # SQLALCHEMY_DATABASE_URI = create_sqlite_uri('app\\static\\database\\voteforher_login.sqlite')
-    SQLALCHEMY_DATABASE_URI = "postgres://evqspvzoibbyph:0ea42790b514f2dee5d1414ed82ef6551fa5057f50002f04bc764ce15cd7a423@ec2-54-86-214-124.compute-1.amazonaws.com:5432/dd7qq3kai8fuk7"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
 
 
 class ProductionConfig(BaseConfig): # 正式環境
-    SQLALCHEMY_DATABASE_URI = "postgres://evqspvzoibbyph:0ea42790b514f2dee5d1414ed82ef6551fa5057f50002f04bc764ce15cd7a423@ec2-54-86-214-124.compute-1.amazonaws.com:5432/dd7qq3kai8fuk7"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
     # SQLALCHEMY_DATABASE_URI = create_sqlite_uri('app\\static\\database\\voteforher_login.sqlite')
 
 # 環境配置更換時
--- manager.py
+++ manager.py
@@ -4,7 +4,7 @@
 from app import create_app
 import os
 
-app = create_app('production') # development/production
+app = create_app('development') # development/production
 port = int(os.environ.get('PORT', 5000))
 # app.run(host='0.0.0.0', port=os.environ.get("PORT", 33507)) # HEROKU部署不能用localhost，PORT也要抓他自己生成的
 app.run(host='0.0.0.0', port=port)
