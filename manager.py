b82c10afa3b7aa7b0973f6cc5f9a3593a1fd2896
nm9140322 <nm9140322@gmail.com>
2022-11-21 18:06:22 +0800

    voteforher

--- manager.py
+++ manager.py
@@ -4,7 +4,7 @@
 from app import create_app
 import os
 
-app = create_app('development') # development/production
+app = create_app('production') # development/production
 port = int(os.environ.get('PORT', 33507))
 # app.run(host='0.0.0.0', port=os.environ.get("PORT", 33507)) # HEROKU部署不能用localhost，PORT也要抓他自己生成的
 app.run(host='0.0.0.0', port=port)
