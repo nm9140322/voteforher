6f380c973e4c907b2dfce365c9be896c9b1c074d
nm9140322 <nm9140322@gmail.com>
2022-11-21 17:36:56 +0800

    voteforher

--- manager.py
+++ manager.py
@@ -5,7 +5,8 @@ from app import create_app
 import os
 
 app = create_app('development') # development/production
-app.run(host='0.0.0.0', port=os.environ.get("PORT", 33507)) # HEROKU部署不能用localhost，PORT也要抓他自己生成的
-# app.run()
+port = int(os.environ.get('PORT', 33507))
+# app.run(host='0.0.0.0', port=os.environ.get("PORT", 33507)) # HEROKU部署不能用localhost，PORT也要抓他自己生成的
+app.run(port=port)
 
 # print(app.url_map) # 透過app.url_map可以查詢專案所有路由清單，可以用來看執行狀況
